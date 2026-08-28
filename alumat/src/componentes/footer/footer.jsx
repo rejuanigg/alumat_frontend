03819b2f8e4f611c72ac90f691be349d37b81143
rejuanigg <juanisnm1995@gmail.com>
2026-08-28 20:57:17 -0300

    refact:change the global styles

--- alumat/src/componentes/footer/footer.jsx
+++ alumat/src/componentes/footer/footer.jsx
@@ -6,10 +6,9 @@ function Footer() {
         <footer className="contenedor">
             <img src={alumatLogo} className='imagen'></img>
             <ul className='contenedor--lista'>
-                <li className='contenedor--lista__item'>Inicio</li>
-                <li className='contenedor--lista__item'>¿Quiénes somos?</li>
-                <li className='contenedor--lista__item'>¿Dónde estamos?</li>
-                <li className='contenedor--lista__item'>Contacto</li>
+                <li><a className='contenedor--lista__item item--hover' href="#">Inicio</a></li>
+                <li><a className='contenedor--lista__item item--hover' href="#">Acerca de Nosotros</a></li>
+                <li><a className='contenedor--lista__item item--hover' href="#">Contactanos</a></li>
             </ul>
             <form className='form--container'>
                 <h4 className='form--title'> Suscribete a noticias de Alumat </h4>
--- alumat/src/componentes/navbar/navbar.jsx
+++ alumat/src/componentes/navbar/navbar.jsx
@@ -1,4 +1,5 @@
 import './navbar.css'
+import '../global-styles/global-styles.css'
 import logo from '../../assets/alumatp.png'
 
 function Navbar() {
@@ -6,9 +7,9 @@ function Navbar() {
         <nav className="navbar--container">
             <img className='navbar--logo' src={logo} alt="" />
             <ol className="navbar--list">
-                <li className="navbar--list__item"><a href="#" className="item--link">Home</a></li>
-                <li className="navbar--list__item"><a href="#" className="item--link">Acerca de nosotros</a></li>
-                <li className="navbar--list__item"><a href="#" className="item--link">Contacto</a></li>
+                <li className="navbar--list__item "><a href="#" className="item--link item--hover">Home</a></li>
+                <li className="navbar--list__item "><a href="#" className="item--link item--hover">Acerca de nosotros</a></li>
+                <li className="navbar--list__item "><a href="#" className="item--link item--hover">Contacto</a></li>
             </ol>
         </nav>
     )
